c6f4f9380dd9c331660a791e3a2e4ac4f2bf66d9
crumac <crumac22@gmail.com>
2015-11-27 12:01:06 -0600

    base de conocimientos
    le voy a agregar mas y algunas caracteristicas

---
+++ sistemaexpertocomputadoras.pl
@@ -0,0 +1,31 @@
+%Estructura: marca, modelo, tipo, uso, precio, cores_CPU, RAM, SO, HD, USB 2.0, USB 3.0.
+
+%Laptop
+
+computadora(lenovo, g40-30, laptop, oficina, 7000, 2, 4, windows_8_1, 500gb, 2, 1). %lap01
+computadora(ghia, qcn3540, laptop, oficina, 6060, 4, 4, windows_8_1, 500gb, 2, 1). %lap02
+computadora(acer, tmb115, laptop, oficina, 4860, 2, 4, linux, 320gb, 2, 1). %lap03
+computadora(dell, inspiron_14_3442, laptop, multimedia, 8550, 4, 4, windows_8_1, 1tb, 2, 1). %lap04
+computadora(hp, 14-af16la, laptop, multimedia, 9000, 4, 6, windows_10, 1tb, 2, 1). %lap05
+computadora(asus, f455la, laptop, multimedia, 10970, 4, 4, windows_8_1, 1tb, 2, 1). %lap06
+computadora(hp, elitebook_840, laptop, disenio, 30100, 8, 16, windows_8_1, 1tb, 2, 1). %lap07
+computadora(toshiba, l55-b5179sm, laptop, disenio, 18920, 4, 8, windows_8_1, 1tb, 2,1). %lap08
+computadora(apple, macbookpro, laptop, disenio, 52000, 8, 16, OS_X, 128gb, 0, 2). %lap09
+computadora(lenovo, y50-70, laptop, gamer, 18000, 8, 8, windows_8_1, 1tb, 2, 1). %lap10
+computadora(dell, alienware_15_r2, laptop, gamer, 33100, 8, 8, windows_10, 1tb, 0, 3). %lap11
+computadora(hp,envy_14_u290la,notebook,multimedia,12000,4,6,windows_8_1,1tb,1,2). %lap12
+
+%Escritorio
+
+computadora(dell, inspiron_3646, escritorio, oficina, 7260, 2, 4, windows_8_1, 500gb, 4, 1). %esc01
+computadora(ghia, pentium_dual_g2030, escritorio, oficina, 4690, 2, 4, linux, 500gb, 4, 0). %esc02
+computadora(dell, inspiron_3043, escritorio, oficina, 6180, 2, 2, windows_8_1, 500gb, 3, 0). %esc03
+computadora(lenovo, thinkcentre, escritorio, multimedia, 12020, 4, 4, windows_8_1, 1tb, 3, 3). %esc04
+computadora(hp, prodesk_400, escritorio, multimedia, 10500, 4, 8, windows_8_1, 500gb, 6, 2). %esc05
+computadora(acer, vx2631g, escritorio, multimedia, 10600, 4, 4, windows_7, 500gb, 4, 2). %esc06
+computadora(dell, optiplex_9020, escritorio, disenio, 17380, 8, 8, windows_8_1, 1tb, 2, 6). %esc07
+computadora(lenovo, m53p, escritorio, disenio, 12470, 8, 8, windows_7, 1tb, 2, 6). %esc08
+computadora(apple, imac, escritorio, disenio, 48000, 4, 8, OS_X, 2tb, 0, 4). %esc09
+computadora(ghia, ci7_4790, escritorio, gamer, 12200, 8, 8, windows_8_1, 2tb, 2, 8). %esc10
+computadora(hp, elite_one_800, escritorio, gamer, 24900, 8, 8, windows_8_1, 1tb, 4, 4). %esc11
+
