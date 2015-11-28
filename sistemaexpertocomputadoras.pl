1721189d431b4d542aa14d9ec9a37ce163d8e8b7
Miguel Bobadilla <mbobadilla@outlook.com>
2015-11-27 21:08:51 -0600

    Reglas y formato de resultado.
    º Se agregaron reglas para dos modelos más de laptop.
    
    º Se agregaron todos los datos de la computadora a la salida del
    resultado y se muestran en forma de lista.

--- sistemaexpertocomputadoras.pl
+++ sistemaexpertocomputadoras.pl
@@ -1,4 +1,4 @@
-%Esta línea es para abrir en archivo en OS X.
+%Esta línea es para abrir el archivo en OS X.
 %consult('/Users/fernando2/Github/SistemaExperto/sistemaexpertocomputadoras.pl').
 
 /*
@@ -9,63 +9,111 @@
 	Pérez Rodríguez José Rubén.
 */
 
-%Estructura: marca, modelo, tipo, uso, precio, cores_CPU, RAM, SO, HD, USB 2.0, USB 3.0.
-
-%Laptop
-/*
-computadora(lenovo, g40-30, laptop, oficina, 7000, 2, 4, windows_8_1, 500gb, 2, 1). %lap01
-computadora(ghia, qcn3540, laptop, oficina, 6060, 4, 4, windows_8_1, 500gb, 2, 1). %lap02
-computadora(acer, tmb115, laptop, oficina, 4860, 2, 4, linux, 320gb, 2, 1). %lap03
-computadora(dell, inspiron_14_3442, laptop, multimedia, 8550, 4, 4, windows_8_1, 1tb, 2, 1). %lap04
-computadora(hp, 14-af16la, laptop, multimedia, 9000, 4, 6, windows_10, 1tb, 2, 1). %lap05
-computadora(asus, f455la, laptop, multimedia, 10970, 4, 4, windows_8_1, 1tb, 2, 1). %lap06
-computadora(hp, elitebook_840, laptop, disenio, 30100, 8, 16, windows_8_1, 1tb, 2, 1). %lap07
-computadora(toshiba, l55-b5179sm, laptop, disenio, 18920, 4, 8, windows_8_1, 1tb, 2,1). %lap08
-computadora(apple, macbookpro, laptop, disenio, 52000, 8, 16, OS_X, 128gb, 0, 2). %lap09
-computadora(lenovo, y50-70, laptop, gamer, 18000, 8, 8, windows_8_1, 1tb, 2, 1). %lap10
-computadora(dell, alienware_15_r2, laptop, gamer, 33100, 8, 8, windows_10, 1tb, 0, 3). %lap11
-computadora(hp,envy_14_u290la,notebook,multimedia,12000,4,6,windows_8_1,1tb,1,2). %lap12
-
-%Escritorio
-
-computadora(dell, inspiron_3646, escritorio, oficina, 7260, 2, 4, windows_8_1, 500gb, 4, 1). %esc01
-computadora(ghia, pentium_dual_g2030, escritorio, oficina, 4690, 2, 4, linux, 500gb, 4, 0). %esc02
-computadora(dell, inspiron_3043, escritorio, oficina, 6180, 2, 2, windows_8_1, 500gb, 3, 0). %esc03
-computadora(lenovo, thinkcentre, escritorio, multimedia, 12020, 4, 4, windows_8_1, 1tb, 3, 3). %esc04
-computadora(hp, prodesk_400, escritorio, multimedia, 10500, 4, 8, windows_8_1, 500gb, 6, 2). %esc05
-computadora(acer, vx2631g, escritorio, multimedia, 10600, 4, 4, windows_7, 500gb, 4, 2). %esc06
-computadora(dell, optiplex_9020, escritorio, disenio, 17380, 8, 8, windows_8_1, 1tb, 2, 6). %esc07
-computadora(lenovo, m53p, escritorio, disenio, 12470, 8, 8, windows_7, 1tb, 2, 6). %esc08
-computadora(apple, imac, escritorio, disenio, 48000, 4, 8, OS_X, 2tb, 0, 4). %esc09
-computadora(ghia, ci7_4790, escritorio, gamer, 12200, 8, 8, windows_8_1, 2tb, 2, 8). %esc10
-computadora(hp, elite_one_800, escritorio, gamer, 24900, 8, 8, windows_8_1, 1tb, 4, 4). %esc11
-*/
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Lo mismo, pero con los llamados a las reglas.
-
 %Predicado final que imprime el resultado.
-comprar:- computadora(Modelo),
+comprar:- computadora(Marca, Modelo, Tipo, Uso, Precio, CoresCPU, RAM, SO, HDD, USB2_0, USB3_0),
 	write('La computadora que deberia elegir es: '),
+	nl,
+	write('Marca: '),
+	write(Marca),
+	nl,
+	write('Modelo: '),
 	write(Modelo),
+	nl,
+	write('Tipo: '),
+	write(Tipo),
+	nl,
+	write('Uso recomendado: '),
+	write(Uso),
+	nl,
+	write('Precio: $'),
+	write(Precio),
+	nl,
+	write('Nucleos de CPU: '),
+	write(CoresCPU),
+	nl,
+	write('Memoria RAM: '),
+	write(RAM),
+	nl,
+	write('Sistema operativo: '),
+	write(SO),
+	nl,
+	write('Espacio de almacenamiento: '),
+	write(HDD),
+	nl,
+	write('Cantidad de puertos USB 2.0: '),
+	write(USB2_0),
+	nl,
+	write('Cantidad de puertos USB 3.0: '),
+	write(USB3_0),
+	nl,
+	limpiaBC.
+%Si ninguna computadora cumple los requerimientos, mostramos este mensaje :(
+comprar:-
+	write('Lo sentimos. No contamos con un producto con esas caracteristicas'),
 	nl,
 	limpiaBC.
 
 %Hipótesis.
-%computadora(acer, tmb115, laptop, oficina, 4860, 2, ram_4gb, linux, hdd_320gb, 2, 1):- acerTmb115, !.
-%computadora(apple, macbookpro, laptop, disenio, 52000, 8, ram_16gb, osx, sdd_128gb, 0, 2):- macbookpro, !.
-computadora(macbookpro):- macbookpro, !.
-computadora(desconocido).
+%Estructura: marca, modelo, tipo, uso, precio, cores_CPU, RAM, SO, HD, USB 2.0, USB 3.0.
 
-%Reglas que identifican a cada computadora.
+%El ! al final indice un corte. Esto es para que una vez que se llegó al resultado correcto, no se sigan evaluando otras hipótesis.
+
+%Laptop
+%computadora(lenovo, g40-30, laptop, oficina, 7000, 2, 4, windows_8_1, 500gb, 2, 1).
+%computadora(ghia, qcn3540, laptop, oficina, 6060, 4, 4, windows_8_1, 500gb, 2, 1).
+%computadora(dell, inspiron_14_3442, laptop, multimedia, 8550, 4, 4, windows_8_1, 1tb, 2, 1).
+%computadora(hp, 14-af16la, laptop, multimedia, 9000, 4, 6, windows_10, 1tb, 2, 1).
+%computadora(asus, f455la, laptop, multimedia, 10970, 4, 4, windows_8_1, 1tb, 2, 1).
+%computadora(hp, elitebook_840, laptop, disenio, 30100, 8, 16, windows_8_1, 1tb, 2, 1).
+%computadora(toshiba, l55-b5179sm, laptop, disenio, 18920, 4, 8, windows_8_1, 1tb, 2,1).
+%computadora(lenovo, y50-70, laptop, gamer, 18000, 8, 8, windows_8_1, 1tb, 2, 1).
+%computadora(dell, alienware_15_r2, laptop, gamer, 33100, 8, 8, windows_10, 1tb, 0, 3).
+computadora('HP',	'Envy 14 u290la',	'Laptop', 'Multimedia', '12000',	'4', '6GB',		'Windows 8.1',	'1TB (HDD)',	'1',	'2'):- hpenvy14, !.
+computadora('Acer', 'TMB115',			'Laptop', 'Oficina', 	'4860',		'2', '4GB', 	'Linux Limpus',	'320GB (HDD)', 	'2', 	'1'):- acertmb115, !.
+computadora('Apple','MacBook Pro',		'Laptop', 'Disenio', 	'32000',	'8', '16GB', 	'OS X', 		'128GB (SSD)', 	'0', 	'2'):- macbookpro, !.
+%computadora('No se encontro un producto que cubra sus necesidades', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A').
+
+%Escritorio
+/*computadora(dell, inspiron_3646, escritorio, oficina, 7260, 2, 4, windows_8_1, 500gb, 4, 1).
+computadora(ghia, pentium_dual_g2030, escritorio, oficina, 4690, 2, 4, linux, 500gb, 4, 0).
+computadora(dell, inspiron_3043, escritorio, oficina, 6180, 2, 2, windows_8_1, 500gb, 3, 0).
+computadora(lenovo, thinkcentre, escritorio, multimedia, 12020, 4, 4, windows_8_1, 1tb, 3, 3).
+computadora(hp, prodesk_400, escritorio, multimedia, 10500, 4, 8, windows_8_1, 500gb, 6, 2).
+computadora(acer, vx2631g, escritorio, multimedia, 10600, 4, 4, windows_7, 500gb, 4, 2).
+computadora(dell, optiplex_9020, escritorio, disenio, 17380, 8, 8, windows_8_1, 1tb, 2, 6).
+computadora(lenovo, m53p, escritorio, disenio, 12470, 8, 8, windows_7, 1tb, 2, 6).
+computadora(apple, imac, escritorio, disenio, 48000, 4, 8, OS_X, 2tb, 0, 4).
+computadora(ghia, ci7_4790, escritorio, gamer, 12200, 8, 8, windows_8_1, 2tb, 2, 8).
+computadora(hp, elite_one_800, escritorio, gamer, 24900, 8, 8, windows_8_1, 1tb, 4, 4).
+*/
+
+%Reglas que identifican a cada computadora (Se deben poner cosas que diferencíen a un modelo del resto).
+%Las que no llevan paréntesis son reglas de clasificación.
+hpenvy14:- laptop,
+			windows,
+			verifica('la usara para ver peliculas, videos y escuchar musica'),
+			muchoAlmacenamiento,
+			verifica('sobrepasaria los $10,000').
+
+acertmb115:- laptop,
+			otroSo,
+			economico,
+			muchoAlmacenamiento.
+
 macbookpro:- laptop,
 			otroSo,
-			verifica(debe_tener_mas_de_4_gb_de_ram),
-			verifica(debe_tener_ssd),
-			verifica(sobrepasaria_los_20000_pesos).
+			verifica('debe tener mas de 4GB de RAM'),
+			verifica('debe tener SSD en lugar de disco duro'),
+			verifica('sobrepasaria los $20,000').
 
-%Reglas de clasificación.
-laptop:- verifica(debe_ser_portatil).
-otroSo:- verifica(puede_tener_un_sistema_operativo_distinto_a_windows).
+%Reglas de clasificación (son como características genéricas que pueden tener varios modelos).
+%Se puede poner varias veces la misma regla con distinto parámetro en verifica().
+%Se puede poner más de una condición en cada regla, separándolas por comas.
+laptop:- verifica('debe ser portatil').
+otroSo:- verifica('puede tener un sistema operativo distinto a Windows').
+economico:- verifica('debe ser un equipo economico ($5,000 o menos)').
+muchoAlmacenamiento:- verifica('debe tener gran cantidad de almacenamiento').
+windows:- verifica('debe tener Windows').
 
 
 %Preguntas al usuario.
